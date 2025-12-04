b1db9832c7ce211fb483e82771cdd37e53541673
Nicolas Guasch <nic.guasch@gmail.com>
2025-12-04 17:48:17 +0000

    Refactor day 4 solution in C++ to use a Grid struct for better abstraction and readability.

--- solutions/day-4/c++/day4.cpp
+++ solutions/day-4/c++/day4.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <filesystem>
 #include <format>
 #include <fstream>
@@ -9,68 +10,81 @@
 
 constexpr int DELTA_I[8] = {-1, -1, -1, +0, +1, +1, +1, +0},
               DELTA_J[8] = {-1, +0, +1, +1, +1, +0, -1, -1};
-int countAdjacentRolls(int i, int j, std::vector<std::string> &diagram) {
+
+struct Grid {
+    std::vector<std::string> data;
+
+    Grid(std::vector<std::string> _data) : data(std::move(_data)) {}
+
+    auto operator[](this auto &&self, int i, int j) -> decltype(auto) {
+        return self.data[i][j];
+    }
+
+    bool inBounds(int i, int j) const {
+        return 0 <= i && i < ssize(data) && 0 <= j && j < ssize(data[i]);
+    }
+
+    auto positions() const {
+        return std::views::cartesian_product(
+            std::views::iota(0, static_cast<int>(ssize(data))),
+            std::views::iota(0, static_cast<int>(ssize(data[0]))));
+    }
+
+    auto neighbors(int i, int j) const {
+        return std::views::iota(0, 8) | std::views::transform([=](int k) {
+                   return std::pair{i + DELTA_I[k], j + DELTA_J[k]};
+               }) |
+               std::views::filter([this](auto pos) {
+                   return inBounds(pos.first, pos.second);
+               });
+    }
+};
+int countAdjacentRolls(int i, int j, const Grid &diagram) {
     int rolls = 0;
-    for (int k = 0; k < 8; k++) {
-        int ii = i + DELTA_I[k], jj = j + DELTA_J[k];
-        rolls += 0 <= ii && ii < ssize(diagram) && 0 <= jj &&
-                 jj < ssize(diagram) && diagram[ii][jj] == '@';
+    for (auto [ii, jj] : diagram.neighbors(i, j)) {
+        rolls += diagram[ii, jj] == '@';
     }
     return rolls;
 }
 
-bool accessible(int i, int j, std::vector<std::string> &diagram) {
+bool accessible(int i, int j, const Grid &diagram) {
     return countAdjacentRolls(i, j, diagram) < 4;
 }
 
-bool just_unlocked(int i, int j, std::vector<std::string> &diagram) {
+bool just_unlocked(int i, int j, const Grid &diagram) {
     return countAdjacentRolls(i, j, diagram) == 3;
 }
 
 std::string part1(std::ifstream &inputFile) {
-    std::vector<std::string> diagram =
-        std::views::istream<std::string>(inputFile) |
-        std::ranges::to<std::vector>();
-    int accessibleRolls = 0;
-    for (int i = 0; i < std::ssize(diagram); i++) {
-        for (int j = 0; j < std::ssize(diagram[i]); j++) {
-            accessibleRolls +=
-                diagram[i][j] == '@' && accessible(i, j, diagram);
-        }
-    }
+    Grid diagram = {std::views::istream<std::string>(inputFile) |
+                    std::ranges::to<std::vector>()};
+    auto accessibleRolls =
+        std::ranges::count_if(diagram.positions(), [&](auto pos) {
+            auto [i, j] = pos;
+            return diagram[i, j] == '@' && accessible(i, j, diagram);
+        });
     return std::format("There are {} rolls of paper accessible by forklift.",
                        accessibleRolls);
 }
 
-struct pos {
-    int i;
-    int j;
-};
-
 std::string part2(std::ifstream &inputFile) {
-    std::vector<std::string> diagram =
-        std::views::istream<std::string>(inputFile) |
-        std::ranges::to<std::vector>();
+    Grid diagram = {std::views::istream<std::string>(inputFile) |
+                    std::ranges::to<std::vector>()};
 
     int accessibleRolls = 0;
-    std::queue<pos> toRemove;
-    for (int i = 0; i < ssize(diagram); i++) {
-        for (int j = 0; j < ssize(diagram); j++) {
-            if (diagram[i][j] == '@' && accessible(i, j, diagram))
-                toRemove.emplace(i, j);
-        }
+    std::queue<std::pair<int, int>> toRemove;
+    for (auto [i, j] : diagram.positions()) {
+        if (diagram[i, j] == '@' && accessible(i, j, diagram))
+            toRemove.emplace(i, j);
     }
 
     while (!toRemove.empty()) {
         accessibleRolls++;
         auto [i, j] = toRemove.front();
         toRemove.pop();
-        diagram[i][j] = '.';
-        for (int k = 0; k < 8; k++) {
-            int ii = i + DELTA_I[k], jj = j + DELTA_J[k];
-            if (0 <= ii && ii < ssize(diagram) && 0 <= jj &&
-                jj < ssize(diagram[i]) && diagram[ii][jj] == '@' &&
-                just_unlocked(ii, jj, diagram))
+        diagram[i, j] = '.';
+        for (auto [ii, jj] : diagram.neighbors(i, j)) {
+            if (diagram[ii, jj] == '@' && just_unlocked(ii, jj, diagram))
                 toRemove.emplace(ii, jj);
         }
     }

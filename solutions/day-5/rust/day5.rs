a93d0b9d3a83c03e758e0a39a9e084d483978009
Nicolas Guasch <nic.guasch@gmail.com>
2025-12-07 21:50:18 +0000

    Change parsing function to return iterators. In part 1, sweep with peekable iterator.

--- solutions/day-5/rust/day5.rs
+++ solutions/day-5/rust/day5.rs
@@ -1,40 +1,44 @@
 use std::fs;
 
-fn parse_input(input: &str) -> (Vec<(i64, i64)>, Vec<i64>) {
+fn parse_input(
+    input: &str,
+) -> (
+    impl Iterator<Item = (i64, i64)> + '_,
+    impl Iterator<Item = i64> + '_,
+) {
     let (ranges, ids) = input
         .split_once("\n\n")
         .expect("Failed to split ranges and ids");
     let ranges = ranges
         .lines()
-        .map(|line| {
-            let (l, r) = line.split_once("-").expect("Failed to split range");
+        .map(|line| line.split_once("-").expect("Failed to split range"))
+        .map(|(l, r)| {
             (
                 l.parse::<i64>().expect("Failed to parse l from range"),
                 r.parse::<i64>().expect("Failed to parse r from range"),
             )
-        })
-        .collect();
+        });
     let ids = ids
         .lines()
-        .map(|line| line.parse::<i64>().expect("Failed to parse id"))
-        .collect();
+        .map(|line| line.parse::<i64>().expect("Failed to parse id"));
     (ranges, ids)
 }
 
 fn part1(input: &str) -> i64 {
-    let (mut ranges, mut ids) = parse_input(input);
-    ranges.sort();
-    ids.sort();
+    let (ranges, ids) = parse_input(input);
+    let mut ranges = ranges.collect::<Vec<(i64, i64)>>();
+    let mut ids = ids.collect::<Vec<i64>>();
+    ranges.sort_unstable();
+    ids.sort_unstable();
 
     let mut fresh = 0;
-    let mut id_pos = 0;
-    for range in ranges {
-        let (l, r) = range;
-        while id_pos < ids.len() && ids[id_pos] < l {
-            id_pos += 1;
+    let mut id_iter = ids.into_iter().peekable();
+    for (l, r) in ranges {
+        while id_iter.peek().is_some_and(|&id| id < l) {
+            id_iter.next();
         }
-        while id_pos < ids.len() && ids[id_pos] <= r {
-            id_pos += 1;
+        while id_iter.peek().is_some_and(|&id| id <= r) {
+            id_iter.next();
             fresh += 1;
         }
     }
@@ -42,20 +46,22 @@ fn part1(input: &str) -> i64 {
 }
 
 fn part2(input: &str) -> i64 {
-    let (mut ranges, _ids) = parse_input(input);
-    ranges.sort();
+    let (ranges, _ids) = parse_input(input);
+    let mut ranges = ranges.collect::<Vec<(i64, i64)>>();
+    ranges.sort_unstable();
 
-    let (fresh_count, _max_id) = ranges
-        .iter()
-        .fold((0, i64::MIN), |(count, max_seen), range| {
-            let &(l, r) = range;
-            if r > max_seen {
-                let l = std::cmp::max(l, max_seen + 1);
-                (count + r - l + 1, r)
-            } else {
-                (count, max_seen)
-            }
-        });
+    let (fresh_count, _max_id) =
+        ranges
+            .into_iter()
+            .fold((0, i64::MIN), |(count, max_seen), range| {
+                let (l, r) = range;
+                if r > max_seen {
+                    let l = l.max(max_seen + 1);
+                    (count + r - l + 1, r)
+                } else {
+                    (count, max_seen)
+                }
+            });
     fresh_count
 }
 
